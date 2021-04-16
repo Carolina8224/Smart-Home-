b6d41c5bb1b2db56fff7113edbf73111e7c0be83
Anlly Carolina Bravo Saldarriaga <80723688+Carolina8224@users.noreply.github.com>
2021-04-16 14:16:01 -0500

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+sistema de claves de cuatro digitos y una letra mayuscula para el usuario 
+
+*******************************************************************************/
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int n1,n2,n3,n4;
+    int x;
+    int n;
+ cout<<"n1";
+ cin>>n1;
+  cout<<"n2";
+ cin>>n2;
+  cout<<"n3";
+ cin>>n3;
+ cout<<"n4";
+ cin>>n4;
+ cout<<" n1 n1 n2 n3 n4";
+ cout<<"x";
+ cin>>x;
+ if(x>=60 and x<=95)
+ {
+     n+x;
+     cout<<" combinacion  corecta ===>"<<n+x ;
+ }
+ else 
+ {
+ cout<<"***combinacion invalida***";
+ }
+    return 0;
+}
